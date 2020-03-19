c12721c6ca3c0209f38fa3951427dec353b28bc7
Cosma Catalin <cosmacatalin98@yahoo.com>
2020-03-19 13:49:37 +0200

    Add InsertNewProduct method

--- src/main/java/mypackage/Controller.java
+++ src/main/java/mypackage/Controller.java
@@ -11,21 +11,42 @@ public class Controller {
     public Controller() {
     }
 
-    //Doar de test , returneaza "Hello from Product Finder Application !" in Postman
-    @GetMapping("/test") // localhost:8080/test
-    public String getHelloWorld() {
+    /**
+     * Aceasta metoda este folosita pentru a testa
+     * conexiunea la Postman folosind adresa
+     * localhost:8080/test
+     *
+     * @return String Returneaza un mesaj de test
+     */
+    @GetMapping("/test")
+    public String GetHelloWorld() {
         return "Hello from Product Finder Application !";
     }
 
-    //Returneaza in Postman  o lista cu toate obiectele din baza de date din tabela Produse
-    @GetMapping("/all") // localhost:8080/all
-    public List<Object> getAllProducts() {
+    /**
+     * Aceasta metoda ne returneaza in Postman  o lista cu toate obiectele
+     * din baza de date din tabela Produse folosind adresa
+     * http://localhost:8080/allProducts
+     *
+     * @return List<Object> Returneaza o lista cu toate produsele.
+     */
+    @GetMapping("/allProducts")
+    public List<Object> GetAllProducts() {
         return ProductBLL.ViewAllProducts();
     }
 
+    /**
+     * Aceasta metoda ne insereaza un produs nou primit ca si
+     * parametru din Postman folosind adresa
+     * http://localhost:8080/insertNewProduct
+     *
+     * @param prod Produsul primit ca si parametru in format json
+     * @return String Returneaza obiectul ca si String pe post de mesaj
+     * de confirmare.
+     */
     @RequestMapping(value = "/insertNewProduct", method = RequestMethod.POST)
-    public String persistPerson(@RequestBody Product prod) {
-        System.out.println(prod.toString());
+    public String InsertNewProduct(@RequestBody Product prod) {
+        ProductBLL.AddNewProduct(prod);
         return prod.toString();
     }
 
